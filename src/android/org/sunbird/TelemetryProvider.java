fa80e9c5f319195d2d9b44f9728abc8bef9f1d9d
souvikmondal <souvik.mondal@tarento.com>
2018-05-13 09:08:59 +0530

    Issue #SB-3353 fix: build script generalization for extended app

--- src/android/org/sunbird/TelemetryProvider.java
+++ src/android/org/sunbird/TelemetryProvider.java
@@ -1,7 +1,6 @@
 package org.sunbird;
 
 import org.ekstep.genieproviders.telemetry.AbstractTelemetryProvider;
-import org.sunbird.app.BuildConfig;
 
 /**
  * Created by Vinay on 13/06/17.
@@ -10,6 +9,6 @@ import org.sunbird.app.BuildConfig;
 public class TelemetryProvider extends AbstractTelemetryProvider {
     @Override
     public String getPackageName() {
-        return BuildConfig.APPLICATION_ID;
+        return getContext().getApplicationInfo().packageName;
     }
 }
